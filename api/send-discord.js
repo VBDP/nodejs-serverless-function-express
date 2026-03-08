c18df5acd5cdc39caeeb63895b935cb001499095
V <vbd@vbd.es>
2026-03-08 18:00:18 +0100

    Change message and webhook retrieval to query params

--- api/send-discord.js
+++ api/send-discord.js
@@ -7,6 +7,7 @@ export default async function handler(req, res) {
   }
 
   const { message, webhook } = req.body;
+  const { message, webhook } = req.query;
 
   if (!message || !webhook) {
     return res.status(400).send("Faltan parámetros");
